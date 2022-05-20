5bbf56bf1527b12860079024ec89afbf416870fc
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-20 09:59:11 +0530

    feat(Feedback items) - basic component calling written

--- svelte-feedback-app/src/App.svelte
+++ svelte-feedback-app/src/App.svelte
@@ -1,8 +1,35 @@
 <script>
+import FeedbackItem from "./component/FeedbackItem.svelte";
+import FeedbackList from "./component/FeedbackList.svelte";
+
+	let feedBack = [
+		{
+			id: 1,
+			rating: 10,
+			text: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source`
+		},
+		{
+			id: 2,
+			rating: 7,
+			text: `This is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.`
+		},
+		{
+			id: 3,
+			rating: 9,
+			text: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable.`
+		},
+		{
+			id: 4,
+			rating: 5,
+			text: ` Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. `
+		}
+	]
 </script>
 
 <main>
 	<h1>Hello</h1>
+	<FeedbackItem />
+	<FeedbackList />
 </main>
 
 <style>
---
+++ svelte-feedback-app/src/component/FeedbackItem.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<main>
+    feedBack Item
+</main>
