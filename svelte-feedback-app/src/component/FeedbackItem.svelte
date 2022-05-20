939342df936935af970d49f3a78c0200372a2971
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-20 11:04:30 +0530

    feat(Card) - Container Styling added

--- svelte-feedback-app/src/component/FeedbackItem.svelte
+++ svelte-feedback-app/src/component/FeedbackItem.svelte
@@ -10,5 +10,33 @@ import Card from "./Card.svelte";
             {item.rating}
         </div>
         <div class="closeBtn">X</div>
+        <div class="containerText">
+            {item.text}
+        </div>
     </Card>
 </main>
+
+<style>
+  .numberDis {
+    position: absolute;
+    top: -10px;
+    left: -10px;
+    width: 50px;
+    height: 50px;
+    background: #ff6a95;
+    color: #fff;
+    border: 1px #eee solid;
+    border-radius: 50%;
+    padding: 10px;
+    text-align: center;
+    font-size: 19px;
+  }
+  .closeBtn {
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    cursor: pointer;
+    background: none;
+    border: none;
+  }
+</style>
