61846f94dc2d13dd25041cee9fc5b753f5fdea83
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-21 13:03:13 +0530

    feat(Form) - Styling compelete

--- svelte-feedback-app/src/component/FeedbackForm.svelte
+++ svelte-feedback-app/src/component/FeedbackForm.svelte
@@ -29,11 +29,43 @@ import Card from "./Card.svelte";
                 <Button disabled={btnDisabled} type="submit">Post review</Button>
             </div>
         </form>
-        {#if message}
+        <div class="message">
+            {#if message}
             {message}
         {/if}
+        </div>
     </Card>
 </main>
 
 <style>
+      header {
+    max-width: 400px;
+    margin: auto;
+  }
+  header h2 {
+    font-size: 22px;
+    font-weight: 800;
+    text-align: center;
+  }
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+  input:focus {
+    outline: none;
+  }
+  .message{
+    padding-top: 10px;
+    text-align: center;
+    color: rebeccapurple;
+  }
 </style>
