19cf19fa1b6062e2fc697b883b0d625b25bdbd93
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-21 12:19:10 +0530

    feat(Form) - html + check logic written

---
+++ svelte-feedback-app/src/component/FeedbackForm.svelte
@@ -0,0 +1,27 @@
+<script>
+    const minWordLenght = 10
+    let text =''
+    let btnDisabled = true
+    let message
+
+    const lenghtCheck = () => {
+        text.trim().length <= 10 ? (message = `Review should be of atleast ${minWordLenght} charecters`, btnDisabled = true ): (message = null, btnDisabled = false)
+
+    }
+</script>
+
+<main>
+    <header>
+        <h2>How would you rate your service with us ?</h2>
+    </header>
+
+    <form action="">
+        <div class="input-group">
+            <input type="text" placeholder="Tell us something">
+            <button></button>
+        </div>
+    </form>
+</main>
+
+<style>
+</style>
