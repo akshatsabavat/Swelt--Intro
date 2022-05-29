a3b793c60099f44d03b694c67a2d975886ea831a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-29 12:26:17 +0530

    added transitions

--- svelte-feedback-app/src/component/FeedbackList.svelte
+++ svelte-feedback-app/src/component/FeedbackList.svelte
@@ -1,11 +1,14 @@
 <script>
-import FeedbackItem from "./FeedbackItem.svelte";
+    import { fade, scale } from 'svelte/transition'
+    import FeedbackItem from "./FeedbackItem.svelte";
     export let feedBack = [] //defination of a prop
     console.log(feedBack)
 </script>
 
 <main>
     {#each feedBack as fb (fb.id)}
+    <div in:scale out:fade={{duration: 500}}>
     <FeedbackItem item={fb} on:delete-feedback /> <!--Delete Feedback sends the dispatched state-->
+    </div>
     {/each}
 </main>
