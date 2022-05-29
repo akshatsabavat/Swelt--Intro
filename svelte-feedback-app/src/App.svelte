bf7eccba55d952fdb27fcdaf51054db016d62116
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-29 13:18:42 +0530

    defined store.js

--- svelte-feedback-app/src/App.svelte
+++ svelte-feedback-app/src/App.svelte
@@ -2,30 +2,6 @@
 import FeedbackForm from "./component/FeedbackForm.svelte";
 import FeedbackList from "./component/FeedbackList.svelte";
 import FeedbackStats from "./component/FeedbackStats.svelte";
-
-	let feedBack = [
-		{
-			id: 1,
-			rating: 10,
-			text: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source`
-		},
-		{
-			id: 2,
-			rating: 7,
-			text: `This is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.`
-		},
-		{
-			id: 3,
-			rating: 9,
-			text: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable.`
-		},
-		{
-			id: 4,
-			rating: 5,
-			text: ` Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. `
-		}
-	]
-	
 	const deleteFeedback = (e) => {
 		console.log(e.detail)
 		const deletionID = e.detail;
@@ -41,9 +17,9 @@ import FeedbackStats from "./component/FeedbackStats.svelte";
 
 <main class="container">
 	<FeedbackForm on:new-feedback={addFeedback}/>
-	<FeedbackStats count={count} average={average}/>
+	<FeedbackStats/>
 	<!--passing item to prop-->
-	<FeedbackList feedBack={feedBack} on:delete-feedback={deleteFeedback}/>
+	<FeedbackList on:delete-feedback={deleteFeedback}/>
 </main>
 
 <style>
--- svelte-feedback-app/src/component/FeedbackList.svelte
+++ svelte-feedback-app/src/component/FeedbackList.svelte
@@ -1,12 +1,12 @@
 <script>
     import { fade, scale } from 'svelte/transition'
     import FeedbackItem from "./FeedbackItem.svelte";
-    export let feedBack = [] //defination of a prop
+    import { FeedbackStore } from "../FeedbackStore"
     console.log(feedBack)
 </script>
 
 <main>
-    {#each feedBack as fb (fb.id)}
+    {#each $FeedbackStore as fb (fb.id)}
     <div in:scale out:fade={{duration: 500}}>
     <FeedbackItem item={fb} on:delete-feedback /> <!--Delete Feedback sends the dispatched state-->
     </div>
